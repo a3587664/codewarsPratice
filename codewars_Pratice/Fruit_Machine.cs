c1d42133394dd9c4d0409b28d7214060ba4d405e
Sam Chuang <a3587664@gmail.com>
2017-12-03 17:30:41 +0800

    test2 pass

--- codewars_Pratice/Fruit_Machine.cs
+++ codewars_Pratice/Fruit_Machine.cs
@@ -17,14 +17,25 @@ namespace codewars_Pratice
             string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
             var reels = new List<string[]> { reel, reel, reel };
             int[] spins = { 0, 0, 0 };
-            Assert.AreEqual(100, Slot.Fruit(reels, spins));
+            Assert.AreEqual(100, Slot.CalculateScore(reels, spins));
+        }
+
+        [TestMethod]
+        public void Three_Diff()
+        {
+            string[] reel1 = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            string[] reel2 = { "Bar", "Wild", "Queen", "Bell", "King", "Seven", "Cherry", "Jack", "Star", "Shell" };
+            string[] reel3 = { "Bell", "King", "Wild", "Bar", "Seven", "Jack", "Shell", "Cherry", "Queen", "Star" };
+            List<string[]> reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = { 5, 4, 3 };
+            Assert.AreEqual(0, Slot.CalculateScore(reels, spins));
         }
 
         public class Slot
         {
-            public static int Fruit(List<string[]> reels, int[] spins)
+            public static int CalculateScore(List<string[]> reels, int[] spins)
             {
-                return 100;
+                return reels[0][spins[0]] != reels[1][spins[1]] ? 0 : 100;
             }
         }
     }
